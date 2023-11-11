092aadcf43b7e78841dbf83a43e7ad3005be7260
Lucas Marcolongo <lucas_marco@live.com>
2023-11-11 01:33:02 -0800

    feat: enable ssr

--- apps/bff/src/app/app.config.ts
+++ apps/bff/src/app/app.config.ts
@@ -2,7 +2,11 @@ import { ApplicationConfig } from '@angular/core';
 import { provideRouter } from '@angular/router';
 import { appRoutes } from './app.routes';
 import { withComponentInputBinding } from '@angular/router';
+import { provideClientHydration } from '@angular/platform-browser';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(appRoutes, withComponentInputBinding())],
+  providers: [
+    provideClientHydration(),
+    provideRouter(appRoutes, withComponentInputBinding()),
+  ],
 };
--- apps/bff/src/app/services/device-detector.service.ts
+++ apps/bff/src/app/services/device-detector.service.ts
@@ -1,12 +1,27 @@
-import { Injectable } from '@angular/core';
+import { Injectable, Optional, PLATFORM_ID, Inject } from '@angular/core';
+import { isPlatformBrowser } from '@angular/common';
+import { Request } from 'express';
 
 @Injectable({
   providedIn: 'root',
 })
 export class DeviceDetectorService {
+  constructor(
+    @Inject(PLATFORM_ID) private platformId: object,
+    @Optional() @Inject('REQUEST') private request: Request
+  ) {}
+
+  private get userAgent(): string {
+    if (isPlatformBrowser(this.platformId)) {
+      return navigator.userAgent;
+    } else {
+      return this.request.headers['user-agent'] || '';
+    }
+  }
+
   public isMobile(): boolean {
     return /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(
-      navigator.userAgent
+      this.userAgent
     );
   }
 }
